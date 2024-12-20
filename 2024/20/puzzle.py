521b1edeebe5e8ef799abd7ca997d803aac84495
Jean Dubois <jd-dev@laposte.net>
2024-12-20 07:11:51 +0100

    [not working] add day20 WIP

--- 2024/20/puzzle.py
+++ 2024/20/puzzle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+import copy
 
 
 def open_input(input_f: str = "input") -> str:
@@ -37,60 +38,35 @@ def parse(puzzle_input: str) -> tuple[list[list[int]], tuple[int, int], tuple[in
     return maze, start, end
 
 
-def find_best_way(
+def cost_of_tiles(
         maze: list[list[int]], start: tuple[int, int], end: tuple[int, int],
-        nowall: tuple[int, int],
-        allowed_to_tp: tuple[tuple[int, int], tuple[int, int]] = ((-1, -1), (-1, -1)),
-        cheat_cost: int = 0
 ):
-    """Dijkstra"""
-    tiles_to_visit: list[tuple[int, int, int]] = [(*start, 0)]
-    known_tiles: dict[tuple[int, int], int] = {start: 0}
-    maze_h = len(maze)
-    maze_w = len(maze[0])
+    """Determine the cost of each tile and return a 2D list where -1 are walls
+       and other are normal tiles, from 0 (start) to the end"""
+    new_maze = [[-j for j in line] for line in maze]
 
-    while len(tiles_to_visit) > 0:
-        tiles_to_visit.sort(key=lambda x: x[2])
-        i, j, cost = tiles_to_visit.pop(0)
-        neighbours = [(i-1, j, 1), (i+1, j, 1), (i, j+1, 1), (i, j-1, 1)]
-
-        if (i, j) == allowed_to_tp[0]:
-            neighbours.append(allowed_to_tp[1]+(cheat_cost,))
-        if (i, j) == allowed_to_tp[1]:
-            neighbours.append(allowed_to_tp[0]+(cheat_cost,))
-        found_end = False
-
-        for ni, nj, ncost in neighbours:
-            if ni < 0 or nj < 0: continue
-            if ni >= maze_h: continue
-            if nj >= maze_w: continue
-            if maze[ni][nj] == 1 and (ni, nj) != nowall: continue  # wall
-            if (ni, nj) in known_tiles: continue
-
-            known_tiles[(ni, nj)] = cost+ncost
-
-            if (ni, nj) == end:
-                found_end = True
-                break
-            tiles_to_visit.append((ni, nj, cost+ncost))
-        if found_end:
+    i, j = start
+    cost = 0
+    path: list[tuple[int, int]] = [(i, j)]
+    while (i, j) != end:
+        neighbours = [(i-1, j), (i+1, j), (i, j+1), (i, j-1)]
+        for ni, nj in neighbours:
+            if new_maze[ni][nj] == -1: continue
+            if (ni, nj) in path: continue
+            i, j = ni, nj
             break
+        cost += 1
+        new_maze[i][j] = cost
+        path.append((i, j))
 
-    assert end in known_tiles
-    return known_tiles[end]
+    return new_maze, path
 
 
-def solve_puzzle1(maze: list[list[int]], start: tuple[int, int], end: tuple[int, int]) -> int:
+def solve_puzzle1(new_maze: list[list[int]], start: tuple[int, int], end: tuple[int, int], path: list[tuple[int, int]]) -> int:
     """Solves puzzle 1"""
-    normal_solution = find_best_way(maze, start, end, (-1, -1))
-    all_solutions: dict[int, int] = {0: 1}
-    for i, line in enumerate(maze):
-        for j, row in enumerate(line):
-            if row != 1: continue
-            solution_without_wall = normal_solution-find_best_way(maze, start, end, (i, j))
-            if solution_without_wall not in all_solutions:
-                all_solutions[solution_without_wall] = 0
-            all_solutions[solution_without_wall] += 1
+    for (i, j) in path:
+        # TODO: find the walls, check if tiles behind the walls are in path,
+        # substract
 
     total = 0
     for solution in all_solutions.keys():
@@ -105,6 +81,7 @@ def solve_puzzle1(maze: list[list[int]], start: tuple[int, int], end: tuple[int,
 
 puzzle_input = open_input("example1.input")
 maze, start, end = parse(puzzle_input)
+new_maze, path = cost_of_tiles(maze, start, end)
 
 # part1 takes 3:54 to run :)
 # print(solve_puzzle1(maze, start, end))
