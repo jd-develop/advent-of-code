0262f28e91c65018c748a9749900d85f1b9afd4e
Jean Dubois <jd-dev@laposte.net>
2024-12-21 08:04:37 +0100

    day21

--- 2024/21/puzzle.py
+++ 2024/21/puzzle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+import itertools
+import functools
 
 
 def open_input(input_f: str = "input") -> str:
@@ -35,105 +37,74 @@ def tile_to_coord(tile: str, door_code: bool) -> tuple[int, int]:
     }[tile]
 
 
-def sequence(code: str, door_code: bool) -> str:
-    #gap = (3, 0) if door_code else (0, 0)
-    current_tile = tile_to_coord("A", door_code)
-    result: str = ""
+def all_movement_possibilities(startx: int, starty: int, endx: int, endy: int, gap: tuple[int, int]):
+    diffx = endx - startx
+    diffy = endy - starty
+    possibility = ""
+    if diffx < 0:
+        possibility += -diffx*"^"
+    else:
+        possibility += diffx*"v"
+    if diffy < 0:
+        possibility += -diffy*"<"
+    else:
+        possibility += diffy*">"
+
+    all_possibilities = itertools.permutations(possibility, len(possibility))
+
+    real_possibilities: list[str] = []
+    for p in all_possibilities:
+        px, py = startx, starty
+        gap_found = False
+        for c in p:
+            if c == "<": py -= 1
+            elif c == ">": py += 1
+            elif c == "^": px -= 1
+            elif c == "v": px += 1
+            if (px, py) == gap:
+                gap_found = True
+                break
+        if gap_found: continue
+        real_possibilities.append("".join(p))
+
+    if len(real_possibilities) == 0:
+        return ["A"]
+    return [res+"A" for res in real_possibilities]
+
+
+@functools.cache
+def min_len(code: str, robot: int, number_of_robots: int, door_code: bool) -> int:
+    current_pos = tile_to_coord("A", door_code)
+    gap = (3, 0) if door_code else (0, 0)
+    lenght = 0
     for char in code:
-        char_coords = tile_to_coord(char, door_code)
-        if char_coords == current_tile:
-            result += "A"
-            continue
-
-        diffx = char_coords[0] - current_tile[0]
-        diffy = char_coords[1] - current_tile[1]
-
-        #possibility = ""
-        #if diffx < 0:
-        #    possibility += -diffx*"^"
-        #else:
-        #    possibility += diffx*"v"
-        #if diffy < 0:
-        #    possibility += -diffy*"<"
-        #else:
-        #    possibility += diffy*">"
-
-        #all_possibilities = itertools.permutations(possibility, len(possibility))
-        #if care_about_gaps:
-        #    real_possibilities: list[str] = []
-        #    for p in all_possibilities:
-        #        px, py = current_tile
-        #        gap_found = False
-        #        for c in p:
-        #            if c == "<": py -= 1
-        #            elif c == ">": py += 1
-        #            elif c == "^": px -= 1
-        #            elif c == "v": px += 1
-        #            if (px, py) == gap:
-        #                gap_found = True
-        #                break
-        #        if gap_found: continue
-        #        real_possibilities.append("".join(p))
-        #else:
-        #    real_possibilities = list(map(lambda x: "".join(x), all_possibilities))
-
-        #assert len(real_possibilities) != 0, (possibility, list(all_possibilities), current_tile, char_coords)
-
-        #if len(result) == 0:
-        #    result = real_possibilities
-        #    for i in range(len(result)):
-        #        result[i] = result[i]+"A"
-        #else:
-        #    new_result: list[str] = []
-        #    for res in result:
-        #        for possibility in real_possibilities:
-        #            new_result.append(res+possibility+"A")
-        #    result = new_result
-
-        if door_code:
-            if diffx < 0:
-                result += -diffx*"^"
-            if diffy < 0:
-                result += -diffy*"<"
-            else:
-                result += diffy*">"
-            if diffx > 0:
-                result += diffx*"v"
+        target_pos = tile_to_coord(char, door_code)
+        all_moves = all_movement_possibilities(*current_pos, *target_pos, gap)
+        if robot == number_of_robots:
+            lenght += len(min(all_moves, key=len))
         else:
-            if diffx > 0:
-                result += diffx*"v"
-            if diffy < 0:
-                result += -diffy*"<"
-            else:
-                result += diffy*">"
-            if diffx < 0:
-                result += -diffx*"^"
-        result += "A"
-        current_tile = char_coords
-
-    return result
+            new_min_lenght = -1
+            for move in all_moves:
+                cur_len = min_len(move, robot+1, number_of_robots, False)
+                if new_min_lenght == -1 or cur_len < new_min_lenght:
+                    new_min_lenght = cur_len
+            assert new_min_lenght != -1
+            lenght += new_min_lenght
+        current_pos = target_pos
+    return lenght
 
 
-def solve_puzzle1(codes: list[str]) -> int:
+def solve_puzzle1(codes: list[str], part2: bool = False) -> int:
     """Solves puzzle 1"""
     result = 0
     for code in codes:
-        first_robot_sequence = sequence(code, True)
-        second_robot_sequence = sequence(first_robot_sequence, False)
-        my_sequence = sequence(second_robot_sequence, False)
-        print(my_sequence)
-        print(len(my_sequence))
-
-        result += len(my_sequence)*int(code[:-1])
+        result += min_len(code, 0, 2+part2*23, True)*int(code[:-1])
     return result
 
 
-# def solve_puzzle2() -> int:
-#     """Solves puzzle 2"""
-
-puzzle_input = open_input("example1.input")
+puzzle_input = open_input()
 parsed_input = parse(puzzle_input)
 
 print(solve_puzzle1(parsed_input))
-# print(solve_puzzle2(parsed_input))
+print(solve_puzzle1(parsed_input, True))
 
