fb9ae63da47cec0f8d65950d90a274036eec32ba
Jean Dubois <jd-dev@laposte.net>
2025-11-29 18:13:56 +0100

    get ready for Monday

---
+++ 2025/template.ml
@@ -0,0 +1,11 @@
+
+let open_input (input_f: string option) : string list =
+  let real_name = match input_f with
+  | Some s -> s
+  | None -> "input"
+  in
+  let input_channel = open_in real_name in
+  let input_contents =
+    really_input_string input_channel (in_channel_length input_channel) in
+  String.split_on_char '\n' input_contents
+
