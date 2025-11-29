1996ddedf2e4c21cfc29a550d963f94329271040
Jean Dubois <jd-dev@laposte.net>
2025-11-29 18:15:05 +0100

    fix

--- 2025/template.ml
+++ 2025/template.ml
@@ -7,5 +7,6 @@ let open_input (input_f: string option) : string list =
   let input_channel = open_in real_name in
   let input_contents =
     really_input_string input_channel (in_channel_length input_channel) in
+  close_in input_channel;
   String.split_on_char '\n' input_contents
 
